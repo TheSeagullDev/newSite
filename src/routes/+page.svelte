d237a749ede50fe6c54b0e958496364b2f207bbd
TheSeagullDev <njsiegel9@gmail.com>
2025-09-14 20:05:26 -0400

    Add some basic stuff

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import bgImg from '$lib/assets/bgImg.png';
+</script>
+
+<div
+	style="background-image: url({bgImg});"
+	class="flex h-screen items-center justify-center bg-cover"
+>
+	<div class="flex w-1/3 flex-col rounded-xl border-2 bg-blue-100 p-12 shadow-2xl gap-2">
+		<h1 class="text-3xl">Hi there! 👋</h1>
+		<p class="text-md">
+			I'm Noah, but you'll usually see me with the username TheSeagullDev. I'm a high school senior
+			with a passion for coding, 3D printing, sound engineering, and more!
+		</p>
+	</div>
+</div>
